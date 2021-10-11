3f3d0bb881064561f0aeeaa44860def84baa45d5
meangpu <51825872+meangpu@users.noreply.github.com>
2021-10-11 12:01:59 +0700

    Create main.py

---
+++ Assignment_2/scpt/main.py
@@ -0,0 +1,12 @@
+"""
+Assignment2
+Student name: Nattapong Tangsatheanrapap
+613040128-7
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
